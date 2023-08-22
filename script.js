95b1640af51eba52ed64f9e3976cda7bcb33f943
Priyanka Singh Tomer <136883301+PST09@users.noreply.github.com>
2023-08-22 18:40:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function calculateMinCost() {
+ function calculateMinCost() {
   //your code here
+  let ropes = document.getElementById("rope-lengths").value
+	
+	let arr = ropes.split(",");
+	let finalValue =0;
+	while(arr.length > 1){
+		arr.sort((a,b) =>{return(a-b)});
+		let value = parseInt (arr.shift());
+		let value1 = parseInt(arr.shift());
+		let mainValue = value+value1;
+		finalValue = finalValue + mainValue;
+		arr.push(mainValue);
+		
+	}
+  let result = document.getElementById("result");
+	result.innerText = finalValue;
+	return finalValue;
+			
+} //your code here
   
   
   
